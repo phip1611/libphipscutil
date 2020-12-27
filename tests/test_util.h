e02a6cedbfdf24bf901016203066af21af65819e
Philipp Schuster <phip1611@gmail.com>
2020-12-27 23:30:43 +0100

    builds on gcc with -Wpedantic

--- tests/test_util.h
+++ tests/test_util.h
@@ -10,5 +10,5 @@
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 #define FMT_PRFX "TEST FAILED [%15s:%03d] %15s(): "
 #define fail_test(msg) \
-    printf(ANSI_RED FMT_PRFX ANSI_CLEAR"%s\n", __FILENAME__, __LINE__, __FUNCTION__, msg); \
+    printf(ANSI_RED FMT_PRFX ANSI_CLEAR"%s\n", __FILENAME__, __LINE__, __func__, msg); \
     exit(-1);
